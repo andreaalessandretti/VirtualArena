d3e146329c780c45b7caf7c8fd795d0ff52bc5de
Andrea Alessandretti <andrea.alessandretti@gmail.com>
2014-06-17 11:16:31 +0200

    From GPL to FreeBSD license

--- utils/generateSamples.m
+++ utils/generateSamples.m
@@ -14,26 +14,37 @@ function samples = generateSamples( varargin )
 %
 
 
+ 
 % This file is part of VirtualArena.
 %
-% Copyright (C) 2012-14 Andrea Alessandretti
+% Copyright (c) 2014, Andrea Alessandretti
+% All rights reserved.
 %
-% andrea.alessandretti@{ist.utl.pt, epfl.ch}
-% Automatic Control Laboratory, EPFL, Lausanne, Switzerland.
-% Institute System and Robotics, IST, Lisbon, Portugal.
-%
-% This program is free software: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-%
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-%
-% You should have received a copy of the GNU General Public License
-% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+% e-mail: andrea.alessandretti [at] {epfl.ch, ist.utl.pt}
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+% 
+% 1. Redistributions of source code must retain the above copyright notice, this
+%    list of conditions and the following disclaimer. 
+% 2. Redistributions in binary form must reproduce the above copyright notice,
+%    this list of conditions and the following disclaimer in the documentation
+%    and/or other materials provided with the distribution.
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+% ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+% WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+% DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+% ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+% (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+% LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+% ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+% (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+% 
+% The views and conclusions contained in the software and documentation are those
+% of the authors and should not be interpreted as representing official policies, 
+% either expressed or implied, of the FreeBSD Project.
 
 switch varargin{1}
     case 'SquareCanvas'
